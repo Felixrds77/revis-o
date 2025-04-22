eee1ab5dd7f4cf948413c387368107e7d8fbcfc4
Felixrds77 <felixcanarinho@gmail.com>
2025-04-22 08:23:38 -0300

    revisão

---
+++ revisão.py
@@ -0,0 +1,8 @@
+a = float(input("digite um numero: "))
+b = float(input("digite um numero: "))
+c = float(input("digite um numero: "))
+soma = a+b
+if soma>=c:
+    print(f"{soma} é maior que {c}")
+else:
+    print(f"{soma} não é maior que {c}")
